1d4c5a36f21c5bd7e58af4132e01f7f36ab8c19a
youssefxblack98 <75281953+youssefxblack98@users.noreply.github.com>
2022-07-15 15:46:37 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['+201014511691']
+global.premium = ['+201014511691']
+global.ownernomer = '+201014511691'
+global.ownername = 'lEVI-ACKERMAN'
+global.botname = 'LEVI-ACKERMAN'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
